b70c8b959e0156e8a0167005dc12b062e9416fa8
Hervé Frezza-Buet <Herve.Frezza-Buet@centralesupelec.fr>
2023-12-12 22:57:30 +0100

    UPDT: features in rllib2-eigen

--- rllib2-eigen/examples/example-001-001-feature.cpp
+++ rllib2-eigen/examples/example-001-001-feature.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <Eigen/Dense>
 #include <rllib2-eigen.hpp>
+#include <string>
+
+#include <fstream>
+
+using Point2D = Eigen::Vector<double, 2>;
 
 int main(int argc, char* argv[]) {
 
   {
+    std::cout << std::endl << std::string(10, '-') << std::endl << std::endl;
+    
     auto phi = rl2::eigen::feature::polynomial<8> {};
     Eigen::Vector<double, 9> res = phi(2);
     std::cout << "phi(2): " << std::endl << res << std::endl;
@@ -13,16 +20,92 @@ int main(int argc, char* argv[]) {
     auto f = rl2::eigen::linear::make<double, 9>(phi, theta);
     auto g = rl2::eigen::linear::make<double, 9>(phi, std::cref(theta));
 
-    
     std::cout << std::endl
-	      << f(2) << ", " << f(3) << ", " << g(2) << ", " << g(3) << std::endl;
+	      << "f(2) = " << f(2) << ", " << "f(3) = " << f(3) << ", "
+	      << "g(2) = " << g(2) << ", " << "g(3) = " << g(3) << std::endl;
 
     theta[0] = 1;
 
+    std::cout << std::endl
+	      << "f(2) = " << f(2) << ", " << "f(3) = " << f(3) << ", "
+	      << "g(2) = " << g(2) << ", " << "g(3) = " << g(3) << std::endl;
+    
+  }
+
+
+
+  
+  {
+    std::cout << std::endl << std::string(10, '-') << std::endl << std::endl;
+
+    Point2D x {0, 0};
+    auto normal = rl2::eigen::function::gaussian(x, 1.0);
+
+    std::cout << normal({0., 0.}) << ", " << normal({0.1, 0.1}) << std::endl;
+  }
+
+
+  
+  
+  {
+    std::cout << std::endl << std::string(10, '-') << std::endl << std::endl;
+
+    rl2::eigen::feature::gaussian_rbf<double, 3> phi {{1.0, .5},  // mean1, sigma1
+						      {2.0, .5},  // mean2, sigma2
+						      {3.0, .5}}; // mean3, sigma3
+
+    std::cout << std::endl;
+    int i = 1;
+    for(auto& rbf : phi.rbfs) {
+      std::cout << "radial basis #" << i++ << ": rbf(2.0) = " << rbf(2.0) << std::endl;
+    }
+
+    std::cout << std::endl
+	      << phi(2.0) << std::endl
+	      << std::endl
+	      << phi(2.05) << std::endl;
+    
+    Eigen::Vector<double, 4> theta {0, 1, 1, 1};
+    auto f = rl2::eigen::linear::make<double, 4>(phi, theta);
+    auto g = rl2::eigen::linear::make<double, 4>(phi, std::cref(theta));
     
     std::cout << std::endl
-	      << f(2) << ", " << f(3) << ", " << g(2) << ", " << g(3) << std::endl;
+	      << "f(2) = " << f(2) << ", " << "f(3) = " << f(3) << ", "
+	      << "g(2) = " << g(2) << ", " << "g(3) = " << g(3) << std::endl;
+
+    theta[0] = 1;
+
+    std::cout << std::endl
+	      << "f(2) = " << f(2) << ", " << "f(3) = " << f(3) << ", "
+	      << "g(2) = " << g(2) << ", " << "g(3) = " << g(3) << std::endl;
   }
+  
+  {
+    Point2D omega1 {-.5, -.2};
+    Point2D omega2 {  0,   1};
+    
+    rl2::eigen::feature::gaussian_rbf<Point2D, 2> phi {{omega1, .2},  // mean1, sigma1
+						       {omega2, .5}}; // mean2, sigma2
+    Eigen::Vector<double, 3> theta {0, 1, .5};
+    auto f = rl2::eigen::linear::make<Point2D, 3>(phi, theta);
+
+    std::ofstream gnuplot {"demo.plot"};
+    gnuplot << "set hidden3d back" << std::endl
+	    << "set xyplane at 0" << std::endl
+	    << "splot '-' with lines" << std::endl;
+    
+    Point2D M;
+    for(M[1] = -2; M[1] <= 2; M[1] += .075, gnuplot << std::endl)
+      for(M[0] = -2; M[0] <= 2; M[0] += .075)
+	gnuplot << M[1] << ' ' << M[0] << ' ' << f(M) << std::endl;
+
+    std::cout << std::endl << std::endl << std::endl
+	      << "Run:" << std::endl 
+	      << "  gnuplot -p demo.plot" << std::endl
+	      << std::endl;
+  }
+
+
   
   return 0;
 }
--- rllib2-eigen/src/rllib2eigenFunctional.hpp
+++ rllib2-eigen/src/rllib2eigenFunctional.hpp
@@ -1,12 +1,66 @@
 #pragma once
 
+#include <cmath>
+#include <complex>
+#include <array>
+#include <initializer_list>
 #include <Eigen/Dense>
 #include <rllib2eigenConcepts.hpp>
+
 
 namespace rl2 {
   namespace eigen {
 
+    namespace function {
+
+      template<typename DOMAIN>
+      struct Gaussian {
+      private:
+	DOMAIN mu;
+	double sigma;
+	double gamma;
+
+      public:
+	
+	Gaussian(const DOMAIN& mu, double sigma) : mu(mu), sigma(sigma), gamma(.5/(sigma*sigma)) {}
+	Gaussian() : Gaussian({}, 1.) {}
+	Gaussian(const Gaussian&) = default;
+	Gaussian& operator=(const Gaussian&) = default;
+
+	double operator()(const DOMAIN& x) const {
+	  auto delta = x - mu;
+	  return std::exp(-delta.dot(delta) * gamma);
+	}
+      };
+      
+      template<>
+      struct Gaussian<double> {
+      private:
+	double mu;
+	double sigma;
+	double gamma;
+
+      public:
+	
+	Gaussian(const double& mu, double sigma) : mu(mu), sigma(sigma), gamma(.5/(sigma*sigma)) {}
+	Gaussian() : Gaussian(0, 1.) {}
+	Gaussian(const Gaussian&) = default;
+	Gaussian& operator=(const Gaussian&) = default;
+
+	
+
+	double operator()(const double& x) const {
+	  auto delta = x - mu;
+	  return std::exp(- delta * delta * gamma);
+	}
+      };
+
+      template<typename DOMAIN>
+      auto gaussian(const DOMAIN& mu, double sigma) {return Gaussian(mu, sigma);}
+    }
+
     namespace feature {
+      
       template<unsigned int DEGREE>
       struct polynomial {
       public:
@@ -35,13 +89,41 @@ namespace rl2 {
 	  return res;
 	}
       };
+
+      template<typename DOMAIN, unsigned int NB_RBF>
+      struct gaussian_rbf {
+	using rbf_type = function::Gaussian<DOMAIN>;
+	using return_type = Eigen::Vector<double, NB_RBF + 1>;
+	
+	std::array<function::Gaussian<DOMAIN>, NB_RBF> rbfs;
+	
+	gaussian_rbf() = default;
+	gaussian_rbf(const gaussian_rbf&) = default;
+	gaussian_rbf& operator=(const gaussian_rbf&) = default;
+
+	// I would have preferred something like this.. but I do not know how to force size checking at compiling time.
+	// gaussian_rbf(const std::initializer_list<rbf_type>& args) : rbfs(args) {}
+	
+	gaussian_rbf(const std::initializer_list<rbf_type>& args) : rbfs() {
+	  auto it = args.begin();
+	  for(auto& rbf : rbfs) rbf = *(it++);
+	}
+	
+	return_type operator()(const DOMAIN& x) const {
+	  return_type res;
+	  auto res_it = res.begin();
+	  *(res_it++) = 1; // This is the offset.
+	  for(auto& rbf : rbfs) *(res_it++) = rbf(x);
+	  return res;
+	}
+      };
     }
 
     
     namespace linear {
 
       // Consider providing a std::cref(....) expression as param argument.
-      template<typename AMBIENT, int DIM, concepts::feature<AMBIENT, DIM> FEATURE, typename PARAM>
+      template<typename AMBIENT, unsigned int DIM, concepts::feature<AMBIENT, DIM> FEATURE, typename PARAM>
       auto make(const FEATURE& phi, PARAM param) {
 	return [phi, param](const AMBIENT& arg) {
 	  const Eigen::Vector<double, DIM>& p = param; // Required if param is a std::cref
