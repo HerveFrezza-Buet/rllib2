89261bfc6b5eb5e315ae6a5dcfe5cf220f7fa3e5
Hervé Frezza-Buet <Herve.Frezza-Buet@centralesupelec.fr>
2024-02-05 12:06:43 +0100

    updt

--- rllib2-eigen/examples/example-001-001-feature.cpp
+++ rllib2-eigen/examples/example-001-001-feature.cpp
@@ -11,14 +11,17 @@ int main(int argc, char* argv[]) {
 
   {
     std::cout << std::endl << std::string(10, '-') << std::endl << std::endl;
-    
+
+    // Let us use a degree 8 polynomial approximation.
     auto phi = rl2::eigen::feature::polynomial<8> {};
-    Eigen::Vector<double, 9> res = phi(2);
+    Eigen::Vector<double, 9> res = phi(2); // (2^0, 2^1, 2^2, ... 2^8) 
     std::cout << "phi(2): " << std::endl << res << std::endl;
 
+    // From the previous feature function definition, we can build
+    // linear approximators.
     Eigen::Vector<double, 9> theta {0, 0, 0, 1, 0, 0, 0, 0, 0};
-    auto f = rl2::eigen::linear::make<double, 9>(phi, theta);
-    auto g = rl2::eigen::linear::make<double, 9>(phi, std::cref(theta));
+    auto f = rl2::eigen::linear::make<double, 9>(phi, theta);            // The parameter is copied internally.
+    auto g = rl2::eigen::linear::make<double, 9>(phi, std::cref(theta)); // This is sensitive to parameter changes.
 
     std::cout << std::endl
 	      << "f(2) = " << f(2) << ", " << "f(3) = " << f(3) << ", "
@@ -40,6 +43,7 @@ int main(int argc, char* argv[]) {
 
     Point2D x {0, 0};
     auto normal = rl2::eigen::function::gaussian(x, 1.0);
+    // There is type inference here, the Gaussian works for 2D points.
 
     std::cout << normal({0., 0.}) << ", " << normal({0.1, 0.1}) << std::endl;
   }
@@ -50,6 +54,7 @@ int main(int argc, char* argv[]) {
   {
     std::cout << std::endl << std::string(10, '-') << std::endl << std::endl;
 
+    // Here, 'double' makes us work with 1D Gaussians.
     rl2::eigen::feature::gaussian_rbf<double, 3> phi {{1.0, .5},  // mean1, sigma1
 						      {2.0, .5},  // mean2, sigma2
 						      {3.0, .5}}; // mean3, sigma3
@@ -66,8 +71,9 @@ int main(int argc, char* argv[]) {
 	      << phi(2.05) << std::endl;
     
     Eigen::Vector<double, 4> theta {0, 1, 1, 1};
-    auto f = rl2::eigen::linear::make<double, 4>(phi, theta);
-    auto g = rl2::eigen::linear::make<double, 4>(phi, std::cref(theta));
+    auto f = rl2::eigen::linear::make<double, 4>(phi, theta);            // The parameter is copied internally.
+    auto g = rl2::eigen::linear::make<double, 4>(phi, std::cref(theta)); // This is sensitive to parameter changes.
+
     
     std::cout << std::endl
 	      << "f(2) = " << f(2) << ", " << "f(3) = " << f(3) << ", "
@@ -84,11 +90,13 @@ int main(int argc, char* argv[]) {
     Point2D omega1 {-.5, -.2};
     Point2D omega2 {  0,   1};
     
+    // Let us do the same with 2D Gaussians.
     rl2::eigen::feature::gaussian_rbf<Point2D, 2> phi {{omega1, .2},  // mean1, sigma1
 						       {omega2, .5}}; // mean2, sigma2
     Eigen::Vector<double, 3> theta {0, 1, .5};
     auto f = rl2::eigen::linear::make<Point2D, 3>(phi, theta);
 
+    // Let us write a gnuplot file.
     std::ofstream gnuplot {"demo.plot"};
     gnuplot << "set hidden3d back" << std::endl
 	    << "set xyplane at 0" << std::endl
