a7717cde05bc11bea79b35997b50cf31a9e1b38d
Hervé Frezza-Buet <Herve.Frezza-Buet@centralesupelec.fr>
2024-02-22 16:27:47 +0100

    updt

--- rllib2-eigen/src/rllib2-eigen.hpp
+++ rllib2-eigen/src/rllib2-eigen.hpp
@@ -3,3 +3,7 @@
 #include <rllib2eigenConcepts.hpp>
 #include <rllib2eigenCritic.hpp>
 #include <rllib2eigenCheckings.hpp>
+
+/**
+ * @example example-001-001-lspi-cartpole.cpp
+ */
--- rllib2-eigen/src/rllib2eigenCheckings.hpp
+++ rllib2-eigen/src/rllib2eigenCheckings.hpp
@@ -10,20 +10,6 @@
 namespace rl2 {
   namespace eigen {
     namespace checkings {
-
-      using polynomial_feature = feature::polynomial<10>;
-      static_assert(concepts::feature<polynomial_feature, double, 11>);
-
-      using rbf_feature = feature::gaussian_rbf<double, 10>;
-      static_assert(concepts::feature<rbf_feature, double, 11>);
-
-      using linear_function = decltype(function::make_linear(std::declval<rbf_feature>()));
-      static_assert(concepts::linear_function<linear_function, rbf_feature, double, 11>);			
-
-      using S = double;
-      using A = rl2::enumerable::set<char, 3>;
-      using linear_q_discrete_a = feature::discrete_a::linear<S, A, 11, rbf_feature>;
-      static_assert(concepts::linear_function<linear_q_discrete_a, feature::discrete_a::phi<S, A, 11, rbf_feature>, std::pair<S, A>, 3 * 11>);
     }
   }
 }
