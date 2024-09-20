8ad0b8835faff740c6b28b3eb299275c4d990ae5
Hervé Frezza-Buet <herve.frezza-buet@centralesupelec.fr>
2024-09-20 16:17:22 +0200

    rllib2-eigen avec doc exemples

--- rllib2-eigen/src/rllib2-eigen.hpp
+++ rllib2-eigen/src/rllib2-eigen.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+// TODO mettre une license
+
 #include <rllib2.hpp>
 
 #include <rllib2eigenConcepts.hpp>
@@ -8,6 +10,12 @@
 #include <rllib2eigenNuplet.hpp>
 
 /**
- * @example cartpole-defs.hpp
+ * @example cartpole-utils.hpp
+ * @example mountain-car-utils.hpp
+ * @example rbf-utils.hpp
+ * @example trace-utils.hpp
+ * @example example-000-001-trace-csv.cpp
  * @example example-001-001-lspi-cartpole.cpp
+ * @example example-001-002-lspi-mountain-car.cpp
+ * @example example-001-003-lspi-gridworld.cpp
  */
