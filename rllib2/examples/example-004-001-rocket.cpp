add278a485911de92b68e797d209baed0f888cca
Hervé Frezza-Buet <herve.frezza-buet@centralesupelec.fr>
2025-05-03 20:22:51 +0200

    updt

--- rllib2/examples/example-004-001-rocket.cpp
+++ rllib2/examples/example-004-001-rocket.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <iomanip>
 #include <array>
 #include <random>
+#include <algorithm>
+#include <cstddef>
 
 #include <gdyn.hpp>
 #include <rllib2.hpp>
@@ -8,25 +11,49 @@
 #include "discrete-rocket-problem.hpp"
 #include "my_rocket_config.hpp"
 
+#define NB_PASSES 1000
+
 int main(int argc, char* argv[]) {
   std::random_device rd;
   std::mt19937 gen(rd());
 
   // Let us build up the encapsulations of our rocket.
-  gdyn::problem::rocket::parameters params;
+  auto params = make_params();
   auto rocket = gdyn::problem::rocket::system(params);
   auto relative_rocket = gdyn::problem::rocket::relative::system(rocket, [target = params.ceiling_height/2](){return target;});
   auto exposed_rocket  = types::exposed_system(relative_rocket);
-  auto discrete_rocket = types::discrete_system(exposed_rocket);
+  // auto discrete_rocket = types::discrete_system(exposed_rocket); This one is useless here.
 
   // We apply Q-learning to get the best controller, using a simple
   // tabular Q function.
   std::array<double, types::SA::size()> values;
   auto Q = rl2::enumerable::make_two_args_tabular<types::S, types::A>(values.begin());
 
-  // Let us define learning policies.
-  
-  
+  // The idea here is to explore discrete initial SxA situations
+  // systematically. Just for fun, we explore them several time with a
+  // random permutation.
+  std::array<std::size_t, types::SA::size()> permutation;
+  std::size_t idx = 0;
+  for(auto& i : permutation) i = idx++;
+
+  std::cout << std::endl << std::endl
+	    << "Computing Q-learning passes:" << std::endl;
+  for(std::size_t pass = 0; pass < NB_PASSES; ++pass) {
+    std::cout << std::setw(5) << pass+1 << '/' << NB_PASSES << "\r     " << std::flush;
+    std::shuffle(permutation.begin(), permutation.end(), gen);
+    for(auto [init_state, command]
+	  : permutation
+	  | std::views::transform([](auto sa_idx) {types::SA::iterator it {sa_idx}; return *it;})) {
+      relative_rocket = init_state;
+      auto reward = relative_rocket(command);
+      auto next_state = *relative_rocket;
+      // Nota : we do not need discrete_rocket since making transition
+      // will do the cast into dicrete states and actions.
+      rl2::sarsa<types::S, types::A> transition {init_state, command, reward, next_state};
+      
+    }
+  }
+  std::cout << "Done.                   " << std::endl;
 
   return 0;
 }
--- rllib2/examples/my_rocket_config.hpp
+++ rllib2/examples/my_rocket_config.hpp
@@ -1,7 +1,12 @@
 #pragma once
 
+#include "discrete-rocket-problem.hpp"
+
+// This configures the rocket discretization.
 
 using types = rocket::enumerable::types<
   101, -200., 200., // nb errors, min, max
   051,  -50.,  50., // nb speeds, min, max
   15.,   .5>;       // up thrust, simulation step duration (dt).
+
+inline gdyn::problem::rocket::parameters make_params() {return {};}
