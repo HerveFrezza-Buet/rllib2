1e5c931ef96c8ebb44756d65356d5cf741fef7f7
Hervé Frezza-Buet <herve.frezza-buet@centralesupelec.fr>
2025-05-04 12:16:09 +0200

    updt

--- rllib2/examples/my_rocket_config.hpp
+++ rllib2/examples/my_rocket_config.hpp
@@ -5,8 +5,8 @@
 // This configures the rocket discretization.
 
 using types = rocket::enumerable::types<
-  101, -200., 200., // nb errors, min, max
-  051,  -50.,  50., // nb speeds, min, max
-  15.,   .5>;       // up thrust, simulation step duration (dt).
+  100, -100., 100., // nb errors, min, max
+  100, -100., 100., // nb speeds, min, max
+  15.,    1>;       // up thrust, simulation step duration (dt).
 
 inline gdyn::problem::rocket::parameters make_params() {return {};}
