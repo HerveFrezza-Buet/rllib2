e80af38826f789f32fc3963731fed51fe26c446a
Hervé Frezza-Buet <herve.frezza-buet@centralesupelec.fr>
2025-05-04 13:51:44 +0200

    updt

--- rllib2/examples/my_rocket_config.hpp
+++ rllib2/examples/my_rocket_config.hpp
@@ -7,6 +7,6 @@
 using types = rocket::enumerable::types<
   51, -100., 100., // nb errors, min, max
   51, -100., 100., // nb speeds, min, max
-  15.,    .1>;     // up thrust, simulation step duration (dt).
+  15.,    1.>;     // up thrust, simulation step duration (dt).
 
 inline gdyn::problem::rocket::parameters make_params() {return {};}
